6f113e7fc96889322b31b9ed4ab2633eac75421b
Harsh Pujari <42710594+harshpujari@users.noreply.github.com>
2025-10-25 15:45:00 +0000

    Improve split ratio comments in decision tree

--- decision_tree_classification.py
+++ decision_tree_classification.py
@@ -16,6 +16,7 @@ dataset = pd.read_csv('Social_Network_Ads.csv')
 X = dataset.iloc[:, :-1].values
 y = dataset.iloc[:, -1].values
 
+# Using 75% for training and 25% for testing
 # Splitting the dataset into the Training set and Test set
 from sklearn.model_selection import train_test_split
 X_train, X_test, y_train, y_test = train_test_split(X, y, test_size = 0.25, random_state = 0)
