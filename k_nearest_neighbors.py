8ecf0ef8d5dff24efc678ba3c03bfc9c8b7b2ae1
Harsh Pujari <42710594+harshpujari@users.noreply.github.com>
2025-10-26 11:15:00 +0000

    Improve feature scaling comments in K-NN

--- k_nearest_neighbors.py
+++ k_nearest_neighbors.py
@@ -19,6 +19,7 @@ y = dataset.iloc[:, -1].values
 # Splitting the dataset into the Training set and Test set
 from sklearn.model_selection import train_test_split
 X_train, X_test, y_train, y_test = train_test_split(X, y, test_size = 0.25, random_state = 0)
+# Standardizing features to have mean=0 and variance=1
 print(X_train)
 print(y_train)
 print(X_test)
