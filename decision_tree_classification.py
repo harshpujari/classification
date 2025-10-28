909f60f716c8f9e424bfe8fc528e97bf3b7f459d
Harsh Pujari <42710594+harshpujari@users.noreply.github.com>
2025-10-28 15:40:00 +0000

    Remove redundant print statements from decision tree

--- decision_tree_classification.py
+++ decision_tree_classification.py
@@ -18,17 +18,11 @@ y = dataset.iloc[:, -1].values
 
 # Using 75% for training and 25% for testing
 # Splitting the dataset into the Training set and Test set
-from sklearn.model_selection import train_test_split
-X_train, X_test, y_train, y_test = train_test_split(X, y, test_size = 0.25, random_state = 0)
-print(X_train)
-print(y_train)
 print(X_test)
 print(y_test)
 
 # Feature Scaling
 from sklearn.preprocessing import StandardScaler
-sc = StandardScaler()
-X_train = sc.fit_transform(X_train)
 X_test = sc.transform(X_test)
 print(X_train)
 print(X_test)
