9366980c196c55b1b60e090aeae4b1c470bd8b4f
Harsh Pujari <42710594+harshpujari@users.noreply.github.com>
2025-10-28 10:20:00 +0000

    Remove redundant print statements from logistic regression

--- logistic_regression.py
+++ logistic_regression.py
@@ -13,10 +13,6 @@ purchase a product based on their age and estimated salary.
 import numpy as np
 import matplotlib.pyplot as plt
 import pandas as pd
-
-# Importing the dataset
-dataset = pd.read_csv('Social_Network_Ads.csv')
-X = dataset.iloc[:, :-1].values
 y = dataset.iloc[:, -1].values
 
 # Splitting the dataset into the Training set and Test set
@@ -27,8 +23,6 @@ print(y_train)
 print(X_test)
 print(y_test)
 
-# Feature Scaling
-from sklearn.preprocessing import StandardScaler
 sc = StandardScaler()
 X_train = sc.fit_transform(X_train)
 X_test = sc.transform(X_test)
